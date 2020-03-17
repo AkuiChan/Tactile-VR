7c849dddf1c86746193acf0383a2f966aa9d198e
MitchFly <mfly16@student.aau.dk>
2020-03-17 14:07:46 +0100

    Button Implementation
    Buttom Template is made. Need to set up environment however.

---
+++ Med8_Corvid_Backup/Assets/Script/CustomEventScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomEventScript : MonoBehaviour
+{
+    public GameObject NewObject;
+    
+    public void OnCustomButtonPress()
+    {
+        Instantiate(NewObject);
+        // Debug.Log("We pushed our custom button!");
+    }
+}
+/*
+ * using UnityEngine;
+using Valve.VR.InteractionSystem;
+
+public class Events : MonoBehaviour
+{
+    public void OnPress(Hand hand)
+    {
+        Debug.Log("SteamVR Button pressed!");
+    }
+
+    public void OnCustomButtonPress()
+    {
+        Debug.Log("We pushed our custom button!");
+    }
+}
+ * 
+ */
