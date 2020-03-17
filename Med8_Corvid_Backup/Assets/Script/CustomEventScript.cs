dd748fce5a1b56e86325c81ec8277d1d7424e9d5
MitchFly <mfly16@student.aau.dk>
2020-03-17 18:06:26 +0100

    Control panel
    Control panel is ready to go, and can be modular with some minor edits. (in case we want to use it for further testing)

--- Med8_Corvid_Backup/Assets/Script/CustomEventScript.cs
+++ Med8_Corvid_Backup/Assets/Script/CustomEventScript.cs
@@ -4,29 +4,55 @@ using UnityEngine;
 
 public class CustomEventScript : MonoBehaviour
 {
-    public GameObject NewObject;
-    
-    public void OnCustomButtonPress()
-    {
-        Instantiate(NewObject);
-        // Debug.Log("We pushed our custom button!");
-    }
-}
-/*
- * using UnityEngine;
-using Valve.VR.InteractionSystem;
-
-public class Events : MonoBehaviour
-{
-    public void OnPress(Hand hand)
-    {
-        Debug.Log("SteamVR Button pressed!");
-    }
+    public GameObject Object1, Object2, Object3;
 
     public void OnCustomButtonPress()
     {
-        Debug.Log("We pushed our custom button!");
+        //For Debuging Purpose
+        Instantiate(Object1);
+        Instantiate(Object2);
+        Instantiate(Object3);
+    }
+
+    public void Condition2()
+    {
+        if(Object1.activeSelf == false)
+        {
+            Object1.SetActive(true);
+            Object2.SetActive(false);
+            Object3.SetActive(false);
+        }
+        else
+        {
+            Object1.SetActive(false);
+        }
+    }
+
+    public void Condition3()
+    {
+        if(Object2.activeSelf == false)
+        {
+            Object1.SetActive(false);
+            Object2.SetActive(true);
+            Object3.SetActive(false);
+        }
+        else
+        {
+            Object2.SetActive(false);
+        }
+    }
+
+    public void Condition4()
+    {
+        if (Object3.activeSelf == false)
+        {
+            Object1.SetActive(false);
+            Object2.SetActive(false);
+            Object3.SetActive(true);
+        }
+        else
+        {
+            Object3.SetActive(false);
+        }
     }
 }
- * 
- */
