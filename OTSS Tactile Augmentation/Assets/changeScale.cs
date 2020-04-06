6a7d1982c0e832f1fd29cc37cee34ba32b6bb9ff
Tobias Delcour <tobiasdelcour@hotmail.com>
2020-04-06 10:26:56 +0200

    Added Off-the_Shelf Tactile Augmentation Solution
    Can change scale of any gameobject live.

---
+++ OTSS Tactile Augmentation/Assets/changeScale.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeScale : MonoBehaviour
+{
+    [SerializeField]
+    GameObject go;
+    [SerializeField]
+    float height;
+    [SerializeField]
+    float width;
+    [SerializeField]
+    float depth;
+    Vector3 oldScale;
+    Vector3 newScale;
+
+    private void Start()
+    {
+        height = go.transform.localScale.y;
+        width = go.transform.localScale.x;
+        depth = go.transform.localScale.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        newScale = new Vector3(width, height, depth);
+        if (oldScale == newScale)
+        {
+            //do nothing
+        }
+        else
+        {
+            go.transform.localScale = new Vector3(width, height, depth);
+            oldScale = go.transform.localScale = new Vector3(width, height, depth);
+        }
+            
+    }
+}
+
